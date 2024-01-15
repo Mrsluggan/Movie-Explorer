884f251b0cea82519278556f0c9ca4cbcca31307
Mrsluggan <ericflyger@gmail.com>
2024-01-15 14:23:59 +0100

    Sök funktion

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 
 let movieList = document.getElementById("movieList");
 let movieInfo = document.getElementById("movieInfo");
+let testknapp = document.getElementById("test");
+let searchField = document.getElementById("searchField");
 
-
-fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=88d6f906b386ac47c004701d8f545df8")
+fetch("https://api.themoviedb.org/3/search/movie?query=Batman&include_adult=false&language=en-US&page=2&api_key=88d6f906b386ac47c004701d8f545df8")
     .then(res => res.json())
     .then(data => {
         printMovies(data);
     })
+
+function searchMovie(movieName){
+    fetch("https://api.themoviedb.org/3/search/movie?query="+ movieName + "&include_adult=false&language=en-US&page=2&api_key=88d6f906b386ac47c004701d8f545df8")
+    .then(res => res.json())
+    .then(data => {
+        printMovies(data);
+    })
+
+}
+
+
+testknapp.addEventListener("click", () =>{
+    movieList.innerHTML = "";
+    let searchFieldValue = searchField.value;
+
+    searchMovie(searchFieldValue);
+})
 
 function printMovies(movies) {
 
@@ -30,6 +48,7 @@ function printMovieInfo(movie) {
     let movieText = document.createElement("p");
     let movieImg = document.createElement("img");
     let movieDiv = document.createElement("div");
+    console.log("movie", movie);
 
     let movieHeadline = document.createElement("h2")
     movieImg.src = "http://image.tmdb.org/t/p/original/" + movie.poster_path;
