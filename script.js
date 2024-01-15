5b91fcfbd82b7c7585ba311f376200f9d0e7f9ce
Mrsluggan <ericflyger@gmail.com>
2024-01-15 16:35:06 +0100

    page select

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let movieList = document.getElementById("movieList");
 let movieInfo = document.getElementById("movieInfo");
 let testknapp = document.getElementById("test");
 let searchField = document.getElementById("searchField");
+let buttonDiv = document.getElementById("buttonDiv");
 
 fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&&api_key=88d6f906b386ac47c004701d8f545df8")
     .then(res => res.json())
@@ -10,25 +11,44 @@ fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&&api_key
         printMovies(data);
     })
 
-function searchMovie(movieName){
-    fetch("https://api.themoviedb.org/3/search/movie?query="+ movieName + "&include_adult=false&language=en-US&page=2&api_key=88d6f906b386ac47c004701d8f545df8")
-    .then(res => res.json())
-    .then(data => {
-        printMovies(data);
-        console.log(data)
-        let totalPages = document.createElement("div");
-        for (i=1; i<=data.total_pages; i++) {
-            let button = document.createElement("button");
-            button.textContent = i;
-            totalPages.appendChild(button);
-            console.log("funkar");
+function searchMoviePage(movieName, page) {
 
-        }
-        movieList.appendChild(totalPages);
-    })
+    fetch("https://api.themoviedb.org/3/search/movie?query=" + movieName + "&include_adult=false&language=en-US&page=" + page + "&api_key=88d6f906b386ac47c004701d8f545df8")
+        .then(res => res.json())
+        .then(data => {
+            printMovies(data);
+        })
+
+}
+
+function searchMovie(movieName) {
+    buttonDiv.innerHTML ="";
+    fetch("https://api.themoviedb.org/3/search/movie?query=" + movieName + "&include_adult=false&language=en-US&page=1&api_key=88d6f906b386ac47c004701d8f545df8")
+        .then(res => res.json())
+        .then(data => {
+            printMovies(data);
+            console.log(data)
+            let totalPages = document.createElement("div");
+            for (let i = 1; i <= data.total_pages; i++) {
+                let button = document.createElement("button");
+
+                button.textContent = i;
+                button.addEventListener("click", () => {
+                    console.log("HEJ", i);
+                    movieList.innerHTML ="";
+                    searchMoviePage(movieName, i);
+
+                })
+                totalPages.appendChild(button);
+                console.log("funkar");
+
+            }
+            buttonDiv.appendChild(totalPages);
+        })
 
 
 }
+
 
 
 testknapp.addEventListener("click", () => {
@@ -36,7 +56,18 @@ testknapp.addEventListener("click", () => {
     movieInfo.innerHTML = "";
     let searchFieldValue = searchField.value;
 
-    searchMovie(searchFieldValue);
+    if (searchFieldValue == "") {
+        fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&&api_key=88d6f906b386ac47c004701d8f545df8")
+            .then(res => res.json())
+            .then(data => {
+
+                printMovies(data);
+
+            })
+    } else {
+        searchMovie(searchFieldValue);
+
+    }
 })
 
 function printMovies(movies) {
